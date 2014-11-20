149be027da4d9eac6c74c55ec46b11ece517ac44
Nicolas Cherel <nicolas.cherel@telecom-paristech.fr>
2014-11-20 10:19:13 +0100

    Suppression commentaire

--- src/MainTest.java
+++ src/MainTest.java
@@ -7,7 +7,6 @@ public class MainTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Maze laby = new Maze();
 		laby.initFromTextFile("data/labyrinthe.txt");
 		laby.saveToTextFile("data/labyrinthe2.txt");
