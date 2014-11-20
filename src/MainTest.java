ad49c9fb201f737dfe03724c2dc33ddabd0c32f5
Nicolas Cherel <nicolas.cherel@telecom-paristech.fr>
2014-11-20 10:38:25 +0100

    Début du projet à 2

--- src/MainTest.java
+++ src/MainTest.java
@@ -3,13 +3,9 @@ import maze.Maze;
 
 public class MainTest {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		Maze laby = new Maze();
 		laby.initFromTextFile("data/labyrinthe.txt");
-		laby.saveToTextFile("data/labyrinthe2.txt");
 		
 	}
 
